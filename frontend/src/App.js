5b89c8bbe007700033a2abd338bb48bac573ebfc
vipul-mhatre <mhatrevipul1518@gmail.com>
2024-12-04 00:08:43 +0530

    initialised frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Django Immage Hash api
+    </div>
+  );
+}
+
+export default App;
